251b131bb1d86980b7f792bc255b9644f0c33bdc
Mwaleh muturi <alex.mwaleh@andela.com>
2018-02-26 00:40:02 +0300

    Add database connection dj-static

--- picha/settings.py
+++ picha/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 """
 
 import os
-
+import dj_database_url
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'qqzg%=7ir0bfdxtv-dii6m0_+%v+s$px(*lqztg^0($k*4$h)9'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['pichapoint.herokuapp.com']
+ALLOWED_HOSTS = ['pichapoint.herokuapp.com','127.0.0.1']
 
 
 # Application definition
@@ -78,13 +78,8 @@ WSGI_APPLICATION = 'picha.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.9/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES={}
+DATABASES['default'] = dj_database_url.config()
 
 
 # Password validation
