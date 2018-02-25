285124c3895da312e0d4caa36f4a7b7e6ea246a9
Mwaleh muturi <alex.mwaleh@andela.com>
2018-02-25 23:57:28 +0300

    Add static root

--- picha/wsgi.py
+++ picha/wsgi.py
@@ -10,6 +10,6 @@ import os
 from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pichs.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "picha.settings")
 
 application = Cling(get_wsgi_application())
